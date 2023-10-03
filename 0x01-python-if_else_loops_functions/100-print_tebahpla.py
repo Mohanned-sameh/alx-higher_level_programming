87b90e113c719284534f504c0110d7dbe27da6d7
Mohanned-sameh <Mohanned2678@gmail.com>
2023-10-03 17:31:32 +0300

    fixed output again

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i)), end="")
+    if i % 2 == 0:
+        n = chr(i)
+    else:
+        n = chr(i - 32)
+    print("{}".format(n), end="")
