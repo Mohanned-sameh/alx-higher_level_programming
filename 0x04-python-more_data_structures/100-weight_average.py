e079e7faf5fe3f657a300630b732d1a1d9f51ce1
Mohanned-sameh <mohanned2678@gmail.com>
2023-10-11 21:09:37 +0300

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == []:
-        return 0
-    else:
+    if my_list:
         return sum(my_list[0]) / sum(my_list[1])
+    else:
+        return 0
