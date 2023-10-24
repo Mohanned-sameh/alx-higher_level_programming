da003c2920560fb9add375cebe524ab0895b932e
Mohanned-sameh <mohanned2678@gmail.com>
2023-10-24 15:54:37 +0300

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A Square class
+"""
+
+
+class Square:
+    """
+    square have size
+    inizialized with no type/value
+    """
+
+    def __init__(self, size):
+        self.__size = size
