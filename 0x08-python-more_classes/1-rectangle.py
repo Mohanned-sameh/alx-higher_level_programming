cbd4ce689d35bedbeaae28fae4abb790e444389e
Mohanned-sameh <mohanned2678@gmail.com>
2023-10-30 20:31:07 +0200

    moved properties to be together

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,6 +26,14 @@ class Rectangle:
         """
         return self.__width
 
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle.
+        Returns:
+            The height of the rectangle.
+        """
+        return self.__height
+
     @width.setter
     def width(self, value):
         """Sets the width of the rectangle.
@@ -40,14 +48,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves the height of the rectangle.
-        Returns:
-            The height of the rectangle.
-        """
-        return self.__height
 
     @height.setter
     def height(self, value):
