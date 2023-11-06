910429007d7d5f9592e0d9f17b53a0291d0aef44
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-06 20:32:18 +0200

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+checks if the object is an instance of a class (direclty or indirectly)
+from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    checks if the object is an instance of a class (direclty or indirectly)
+    from the specified class
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
