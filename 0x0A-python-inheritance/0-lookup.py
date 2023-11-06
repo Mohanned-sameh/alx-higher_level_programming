8cd4ebae8b58ae12ba94fc02ff7d7ddbd76225c7
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-06 20:16:22 +0200

    done

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function: lookup
+Return: list object
+"""
+
+
+def lookup(obj):
+    """
+    function that returns object of list
+    """
+    return obj.__dict__
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+ class: MyList
+
+"""
+
+
+class MyList(list):
+    """class with method"""
+
+    pass
+
+    def print_sorted(self):
+        """method to print sorted list"""
+        print(sorted(list(self)))
