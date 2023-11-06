081ab38a2b3982d698b835369600942af9590682
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-06 20:18:40 +0200

    fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-function: lookup
-Return: list object
+Module with the method lookpu
 """
 
 
 def lookup(obj):
+    """Function for return the attributes for an object"""
+
     return dir(obj)
