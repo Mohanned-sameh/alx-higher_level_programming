b9823533f11230c8514c8914fa18ad5c74a459f6
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:07:47 +0200

    added comments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
+    """a script that adds all arguments to a Python list,"""
     try:
         json_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
