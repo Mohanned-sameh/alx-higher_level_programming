0a912b008b3474705685812d9d6854a2e8929ba2
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:21:55 +0200

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+This module adds all arguments to a Python list and then save them to a file
+"""
 import os.path
-from sys import argv
+import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-json_list = []
+def main():
+    filename = "add_item.json"
+    json_list = []
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
+    try:
+        save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+        load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-for index in argv[1:]:
-    json_list.append(index)
-save_to_json_file(json_list, filename)
+        if os.path.exists(filename):
+            json_list = load_from_json_file(filename)
+
+        json_list.extend(sys.argv[1:])
+
+        save_to_json_file(json_list, filename)
+    except ImportError:
+        print("Error: Unable to import the necessary modules.")
+
+
+if __name__ == "__main__":
+    main()
