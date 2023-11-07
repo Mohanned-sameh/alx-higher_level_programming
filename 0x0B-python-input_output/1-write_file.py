1885435542b1187268f532d0904320c4241dac9f
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 19:17:23 +0200

    added comment

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ args: filename , text
 
 
 def write_file(filename="", text=""):
+    """writes specified text within a specified file"""
     with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
