e891196fc8d9faa65b507848feb4448722a6512e
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:08:40 +0200

    idk

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list,"""
+"""
+a script that adds all arguments to a Python list, and then save them to a file
+"""
+
 import sys
 import json
 
@@ -7,7 +10,9 @@ save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
-    """a script that adds all arguments to a Python list,"""
+    """
+    a script that adds all arguments to a Python list, and then save them to a file
+    """
     try:
         json_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
