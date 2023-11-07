23a668c074f495a1d65484f9aa6da3fa960aad62
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 19:14:56 +0200

    fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,8 @@ args: filename , text
 
 def write_file(filename="", text=""):
     """writes in a specific file a given text"""
-    with open(filename, "w") as f:
-        f.write(text)
+    if filename == "" or text == "":
+        return False
+    else:
+        with open(filename, "w") as f:
+            f.write(text)
