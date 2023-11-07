86c4160b20aa88599fa138e7a353cbc656f7ec10
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:26:44 +0200

    fixed var name

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ def main():
     json_list = []
 
     try:
-        savere = __import__("5-save_to_json_file").save_to_json_file
+        save = __import__("5-save_to_json_file").save_to_json_file
         load = __import__("6-load_from_json_file").load_from_json_file
 
         if os.path.exists(filename):
