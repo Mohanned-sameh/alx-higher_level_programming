38a66e6e567ae8f2bc46e6f034b3389a9bc86d78
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:42:07 +0200

    done

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,10 +3,10 @@
 function: class_to_json
 args: filename
 """
-import json
 
 
 def class_to_json(filename):
-    """returns a dict description with simple data struct"""
-    with open(filename, "r") as f:
-        return json.load(f)
+    """
+    returns a dict description with simple data struct
+    """
+    return vars(filename)
