da5f9550026f28aa2990552f04fa790ad9772a08
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 22:08:06 +0200

    done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,48 +3,38 @@
 Reads from standard input and computes metrics
 """
 import sys
-import signal
 
 total_size = 0
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
-}
-line_count = 0
-
-
-def print_statistics():
-    print(f"Total file size: {total_size}")
-    for status_code in sorted(status_code_counts.keys()):
-        count = status_code_counts[status_code]
-        if count > 0:
-            print(f"{status_code}: {count}")
-
-
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+lines_processed = 0
 
 try:
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) >= 8:
-            status_code = int(parts[8])
-            total_size += int(parts[10])
-            status_code_counts[status_code] += 1
-            line_count += 1
+        try:
+            parts = line.split()
+            file_size = int(parts[-1])
+            status_code = int(parts[-2])
 
-        if line_count % 10 == 0:
-            print_statistics()
+            total_size += file_size
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            lines_processed += 1
+
+            if lines_processed % 10 == 0:
+                print("File size: {}".format(total_size))
+                for code in sorted(status_codes):
+                    if status_codes[code] > 0:
+                        print("{}: {}".format(code, status_codes[code]))
+
+        except (ValueError, IndexError):
+            pass
 
 except KeyboardInterrupt:
-    print_statistics()
+    pass
+
+print("File size: {}".format(total_size))
+for code in sorted(status_codes):
+    if status_codes[code] > 0:
+        print("{}: {}".format(code, status_codes[code]))
