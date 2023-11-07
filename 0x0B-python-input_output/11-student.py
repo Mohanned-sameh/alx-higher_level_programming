cab29958f168c90b6a2426e96f8cb0a8ef9a78f5
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 21:48:22 +0200

    done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+class: student
+public instance: first_name, last_name, age
+"""
+
+import json
+
+
+class Student:
+    """init with self, first_name, last_name, age"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Public method: to_json(self)
+    that retrieves a dictionary representation of a Student instance
+    """
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
+
+    """Public method: reload_from_json(self, json"""
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
+        return self.__dict__
