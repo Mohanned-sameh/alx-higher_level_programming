18090aebd7b04d8f48bb51af4197abbeefd603ed
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 19:11:43 +0200

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""
-a function that reads a text file (UTF8) and prints it out to stdout
-"""
+"""a function that reads a text file (UTF8) and prints it out to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
-        print(f.read(), end="")
+    """reads a text file"""
+    if filename == "":
+        return
+    else:
+        with open(filename, "r") as f:
+            print(f.read(), end="")
