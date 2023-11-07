c5e885331f747bd39b5894ce39dd01f55eede96a
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-07 19:27:49 +0200

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
-
 import json
-
-"""
-function: to_json_string
-returns: json representation of an object
-"""
 
 
 def to_json_string(my_obj):
-    """function to represent json of an object"""
-    if my_obj is None:
-        return None
-    else:
-        return json.dumps(my_obj)
+    """function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
