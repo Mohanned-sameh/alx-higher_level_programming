7dee985737037f2e7aa51a77581e63e41f759a15
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 17:18:23 +0200

    fixed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,4 @@
 -- Convert the database character set and collation
--- Convert all tables and their respective fields to utf8mb4
--- Convert all VARCHAR/TEXT fields to utf8mb4
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-SELECT CONCAT('ALTER TABLE `', table_name, '` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;') AS sql_statements
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0';
-SELECT CONCAT('ALTER TABLE `', table_name, '` MODIFY `', column_name, '` ', data_type, '(',
-    character_maximum_length, ') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;') AS sql_statements
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND (data_type = 'varchar' OR data_type = 'text');
+use hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
