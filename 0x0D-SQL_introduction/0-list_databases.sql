90e1f5dec9b1b4e8a4f6e10626153c13f05820de
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 04:47:29 +0200

    done

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- This script lists all database
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- This script deletes the database hbtn_0c_0
+-- If the database hbtn_0c_0 doesn't exist, it doesn't fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
