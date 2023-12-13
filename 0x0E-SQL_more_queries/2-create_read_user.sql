81302315335a503b4ba537078a3a2d04b6feae6c
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 20:51:40 +0200

    done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- creates db hbtn_0c_2 and user user_0d_2
+-- creates db hbtn_0d_2 and user user_0d_2
 -- user have SELECT only
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_0;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0c_0.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_0.* TO 'user_0d_2'@'localhost';
