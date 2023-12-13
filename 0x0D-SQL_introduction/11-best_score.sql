079b898b4540cfcb2d7b558de49e8c8593cdc6fe
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 16:27:11 +0200

    done

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
--- This script lists all records of second_table where
--- score is greater than or equal 10
-SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- List the score greather than or equal to 10
+-- Execute: cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
