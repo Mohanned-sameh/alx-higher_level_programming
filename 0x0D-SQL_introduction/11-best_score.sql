079d427f3055d4c527541bd4a4760034bd64bf90
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 16:24:04 +0200

    fixed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- This script lists all records of second_table where score is greater than or equal 10
-SELECT * FROM second_table WHERE score >= 10;
+SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
