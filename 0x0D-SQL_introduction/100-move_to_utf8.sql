3f4945afdb9e32df4b3450c76d25830d6157570a
mohanned-sameh <mohanned2678@gmail.com>
2023-12-13 17:17:13 +0200

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,12 @@
--- Convert the table character set and collation
--- Convert the field character set and collation
+-- Convert the database character set and collation
+-- Convert all tables and their respective fields to utf8mb4
+-- Convert all VARCHAR/TEXT fields to utf8mb4
 USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT CONCAT('ALTER TABLE `', table_name, '` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;') AS sql_statements
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0';
+SELECT CONCAT('ALTER TABLE `', table_name, '` MODIFY `', column_name, '` ', data_type, '(',
+    character_maximum_length, ') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;') AS sql_statements
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND (data_type = 'varchar' OR data_type = 'text');
