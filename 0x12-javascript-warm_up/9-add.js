47b772ee5f90bdd8b9c4f89d63ef29c986436a3f
mohanned-sameh <mohanned2678@gmail.com>
2024-01-08 15:53:53 +0200

    added function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
+const add = (a, b) => {
+  return parseInt(a) + parseInt(b);
+};
 if (isNaN(argv[2]) && isNaN(argv[3])) {
   console.log('NaN');
 } else {
-  console.log(parseInt(argv[2]) + parseInt(argv[3]));
+  console.log(add(argv[2], argv[3]));
 }
