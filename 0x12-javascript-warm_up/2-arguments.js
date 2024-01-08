2241cf2be0c7504daca6f45f29b7011096ffe1ec
mohanned-sameh <mohanned2678@gmail.com>
2024-01-08 15:19:51 +0200

    fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-// prints a message depending of the number of arguments passed.
-if (process.argv.length - 2 === 0) {
+// Prints a message depending of the number of arguments passed
+
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length - 2 === 1) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
