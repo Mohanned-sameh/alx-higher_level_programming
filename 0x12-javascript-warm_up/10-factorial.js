0d023bcf8d15df026d23488d9d2fe03907d97298
mohanned-sameh <mohanned2678@gmail.com>
2024-01-08 16:48:15 +0200

    done task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // computes and prints a factorial
 
 function factorial(n) {
-  if (isNaN(n) || n === 1 || n === 0) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
   } else {
     return n * factorial(n - 1);
