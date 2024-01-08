e7164c56bea6be0417ce0cc6b73c02c40e4aa453
mohanned-sameh <mohanned2678@gmail.com>
2024-01-08 16:26:33 +0200

    done task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const num = parseInt(argv[2]);
-const fact = (n) => {
-  if (n === 0 || isNaN(n)) {
-    return 1;
-  }
-  return n * fact(n - 1);
-};
+// computes and prints a factorial
 
-console.log(fact(num));
+function factorial(n) {
+  if (isNaN(n) || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
