0d3303a5888c3724aaa293a715275bb0ae06f06e
mohanned-sameh <mohanned2678@gmail.com>
2024-01-10 18:05:33 +0200

    done task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// reverse a list
-exports.eserver = function (list) {
-  list.reduceRight((a, b) => {
-    a.push(b);
-    return a;
-  });
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
