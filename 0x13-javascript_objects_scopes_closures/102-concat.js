de1aa42840513ceba10a45bd29528b17b646da19
mohanned-sameh <mohanned2678@gmail.com>
2024-01-10 18:25:05 +0200

    done task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs').promises;
-const argv = require('process').argv;
-const file = argv[2];
-const content = argv[3];
-fs.writeFile(file, content).catch((err) => console.log(err));
+const { argv } = require('process');
+
+fs.readFile(argv[2], 'utf8')
+  .then((data) => fs.writeFile(argv[4], data, 'utf8'))
+  .catch((err) => console.error(err));
+
+fs.readFile(argv[3], 'utf8')
+  .then((data) => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+  .catch((err) => console.error(err));
