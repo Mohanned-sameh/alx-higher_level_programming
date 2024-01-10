1d635ece823c529be50929bc0973eac72b641213
mohanned-sameh <mohanned2678@gmail.com>
2024-01-10 18:07:43 +0200

    done task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.logMe = function (item) {
-  for (let i = 0; i < item; i++) {
-    console.log(`${i}: ${arguments[i + 1]}`);
+  for (const element in item) {
+    console.log(element + ': ' + item[element]);
   }
 };
