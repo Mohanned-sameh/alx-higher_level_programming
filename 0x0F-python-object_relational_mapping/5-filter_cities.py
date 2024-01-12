1b13a2fed27f7b68dc381be33e7e7930409cc1f3
mohanned-sameh <mohanned2678@gmail.com>
2024-01-12 20:24:50 +0200

    done task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 """
-script that takes in the name of a state
-as an argument and lists all cities of that state,
- using the database hbtn_0e_4_usa
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
 """
 import MySQLdb
 from sys import argv
 
+# The code should not be executed when imported
 if __name__ == "__main__":
-    # creates a connection to the database
+    # make a connection to the database
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
-    # creates a cursor object to execute SQL queries on the database
+
     cur = db.cursor()
-    # executes a SELECT query to retrieve all cities from the database
     cur.execute(
-        """
-        SELECT cities.name FROM cities JOIN states
-        ON cities.state_id = states.id WHERE states.name = %s
-          ORDER BY cities.id
-          """,
+        "SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s",
+        [argv[4]],
     )
-    # fetches all rows from the cursor object and prints them to the console
+
     rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    # close the connction
+    j = []
+    for i in rows:
+        j.append(i[1])
+    print(", ".join(j))
+
+    # Clean up process
     cur.close()
     db.close()
