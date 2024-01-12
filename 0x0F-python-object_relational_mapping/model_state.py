ecb4074955c83c3d37b845f9e17ba3a5a2eeba44
mohanned-sameh <mohanned2678@gmail.com>
2024-01-12 21:24:37 +0200

    done task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Class: State
+links to the MySQL table states
+attribute: id that represents a column of an auto-generated, unique integer,
+that  cant be null and is a primary key
+attribute: name that represents a column of a string with maximum 128
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class State
+    """
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
