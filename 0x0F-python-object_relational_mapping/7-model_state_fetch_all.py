95589b318c190c2cb5a268aaad6583d44140b615
mohanned-sameh <mohanned2678@gmail.com>
2024-01-12 21:29:37 +0200

    done task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,22 +2,23 @@
 """
 script that lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # connects to database
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    # creates engine to talk with database
+    eng = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
     )
-    # cur to talk with the database
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    # fetch data
-    rows = cur.fetchall()
-    # prints rows
-    for row in rows:
-        print(row)
-    # close connection
-    cur.close()
-    db.close()
+    Base.metadata.create_all(eng)
+    # binds session
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    # prints all states in database in ascending order by id.
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
