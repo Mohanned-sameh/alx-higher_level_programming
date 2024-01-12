f47d792ca9c89274ef081e8c58731c131f432be4
mohanned-sameh <mohanned2678@gmail.com>
2024-01-12 20:16:04 +0200

    done task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
+"""
 import MySQLdb
 from sys import argv
 
-"""
-script that takes in arguments and
- displays all values in the states table of hbtn_0e_0_usa
- where name matches the argument. But this time,
- write one that is safe from MySQL injections!
-"""
+# The code should not be executed when imported
 if __name__ == "__main__":
-    # creates a connection to the database
+    # make a connection to the database
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
-    # creates a cursor object
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
-    # executes a query to retrieve data from the states table
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (argv[4],))
-    # fetches all rows from the query result
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
     rows = cur.fetchall()
-    # prints each row
-    for row in rows:
-        print(row)
-    # closes the cursor and database connection
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
