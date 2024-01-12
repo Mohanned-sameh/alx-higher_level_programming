7325c8d62a1be36a4df48540a308e0f0d50b8e87
mohanned-sameh <mohanned2678@gmail.com>
2024-01-12 22:13:52 +0200

    done task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,15 +2,14 @@
 """adds the State object “California”
 with the City “San Francisco”
 to the database hbtn_0e_100_usa"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.schema import Table
 
 if __name__ == "__main__":
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
