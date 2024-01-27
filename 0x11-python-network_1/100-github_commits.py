44acadc557922918840918d1cc4796288d843063
mohanned-sameh <mohanned2678@gmail.com>
2024-01-27 04:17:03 +0200

    Fix bug in login functionality

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """
-Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
-You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/
+Please list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+You must use the GitHub API,
+here is the documentation https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
 """
 
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     for commit in commits[:10]:
         print(
             "{}: {}".format(
-                commit.get("sha"), commit.get("commit").get("author").get("name")
+                commit.get("sha"),
+                commit.get("commit").get("author").get("name"),
             )
         )
