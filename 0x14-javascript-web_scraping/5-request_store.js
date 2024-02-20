77c6bbcce851c83bacccc0f38e436dd45d1cbb19
mohanned-sameh <mohanned2678@gmail.com>
2024-02-20 15:26:25 +0200

    request a url and add it to a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (err) {
+      if (err) {
+        console.error('error:', err);
+      }
+    });
+  }
+});
