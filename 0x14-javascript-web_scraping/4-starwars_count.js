c5d551e55a645c39799d917c91914ad805068eef
mohanned-sameh <mohanned2678@gmail.com>
2024-02-20 15:24:59 +0200

    starwars characters count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    let count = 0;
+    for (const film of JSON.parse(body).results) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
