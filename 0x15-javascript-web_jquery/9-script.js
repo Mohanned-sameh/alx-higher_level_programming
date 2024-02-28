0e37b3a5f9804edbbf2c04c0bfcc96e01e0c859d
mohanned-sameh <mohanned2678@gmail.com>
2024-02-28 18:06:29 +0200

    done task 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+$('document').ready(function () {
+  $.get(
+    'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    function (data, textStatus) {
+      if (textStatus === 'success') {
+        $('DIV#hello').text(data.hello);
+      }
+    }
+  );
+});
