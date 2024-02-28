869c629773abe97c215a66b5ba3b18704e1c7e00
mohanned-sameh <mohanned2678@gmail.com>
2024-02-28 17:59:21 +0200

    done 6 and 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  type: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    $('#character').text(data.name);
+  },
+  error: function (data) {
+    console.log('ERROR: ' + data);
+  },
+});
